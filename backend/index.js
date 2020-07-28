6244b5fef4928f911ceac4eff4d8afecd79d644d
Matthew Bennett <mbennett@redventures.com>
2020-07-28 11:52:56 -0400

    SQL removed. Connection point created

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,23 @@
 const express = require("express");
 const process = require("process");
-const mysql = require("mysql");
+const request = require("request");
 const cors = require("cors");
 
 const app = new express();
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: 'testDB',
-});
 
 // Allow cors
 app.use(cors())
 
-app.get("/echo/:message", (req, res) => {
-  db.query('SELECT ? as result', [req.params.message],
-    (err, results) => {
-      if(err){
-        console.error(err)
-        res.status(500)
-      }
-      res.send(results[0].result);
-    }
-  );
-});
+app.get("/backToFrontConnection", (req, result) => {
+  console.log(req); //Logs in the terminal
+  const resultObject = {text: "You have connected to the backend!"}
+  result.send(resultObject);
 
-app.get("/meta", (req, res) => {
-    db.query('describe Test_Table', (err, results) => {
-        if(err){
-            console.error(err)
-            res.status(500)
-          }
-        res.send(results)
-    })
-})
+  // request('http://ip-api.com/json/', function(data) {
+  //   console.log(data);
+  //   result.send(data);
+  // })
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server started");
