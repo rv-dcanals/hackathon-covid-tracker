e6a91a3003a62437d5434023804d06303592223a
Matthew Bennett <mbennett@redventures.com>
2020-07-30 13:26:42 -0400

    implemented front-back end connection in quiz

--- frontend/src/components/Quiz.js
+++ frontend/src/components/Quiz.js
@@ -7,7 +7,7 @@ import {homeData, maskData, ageData, sexData, protectionData, infectedData} from
 import "../styles/Quiz.css"
 //import "./components.css";
 
-const Quiz = ({closeModal}) => {
+const Quiz = ({origin, closeModal}) => {
 
     const animatedComponents = makeAnimated();
     const [home, setHome] = useState({value: null, label: null});
@@ -16,6 +16,32 @@ const Quiz = ({closeModal}) => {
     const [sex, setSex] = useState({value: null, label: null});
     const [protection, setProtection] = useState([]);
     const [infected, setInfected] = useState({value: null, label: null});
+
+    /*
+     * Sends a post request using the state values to the backend
+     * where the values are updated accordingly
+     * 
+     * @param endpoint = the endpoint of the post request 
+     * @param value = the abbreviation of the state to add the value to
+     * @param results = the results of the quiz in json format
+     */
+    function updateValue(endpoint, value, results) {
+        fetch('http://localhost:3000/' + endpoint, {
+            method: 'POST',
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(
+            {
+                country: 'US', 
+                region: value,
+                quizResults: results
+            }
+            )
+        })
+        .then(response => response.json());
+    };
 
     function submitForm() {
         let formData = {
@@ -39,6 +65,7 @@ const Quiz = ({closeModal}) => {
             firebase.database()
                 .ref("QuizData")
                 .push(formData);
+            updateValue('updateRiskQuiz', origin, formData);
         }
         console.log(formData)
         
@@ -60,7 +87,7 @@ const Quiz = ({closeModal}) => {
             <br></br>
 
             <div>
-            <question>Are you wearing a mask?<br></br></question>
+            <question>When going out, do you wear a mask?<br></br></question>
             <Select
                 components={animatedComponents}
                 options={maskData}
