d883dad8b4ff6814a52e155f3892abd0b5d45436
Matthew Bennett <mbennett@redventures.com>
2020-07-30 13:27:04 -0400

    Added risk quiz update capabilities to backend

--- backend/index.js
+++ backend/index.js
@@ -124,18 +124,191 @@ const incrementUniversalValue = (value) => {
   firebase.database().ref('allData').child(value).set(firebase.database.ServerValue.increment(1));
 }
 
+
+
+//Compare two strings
+const compStrings = (string1, string2) => {
+  return string1.toUpperCase() === string2.toUpperCase();
+}
+
+
 /*
  * 
  * 
  * @param req
  * @param value 
  */
-const saveRiskQuizResult = (req, value) => {
+const saveRiskQuizResult = (req) => {
   //If country is US, increment that value by 1
-  if (req.body.country === 'US') { 
-    const stateQuiz = getState(req).child("events").child("riskQuiz");
+  if (req.body.country === 'US') {
+    //Infected
+    if (compStrings(req.body.quizResults.infected,'yes')) {
+      getState(req).child("events").child("riskQuiz").child("knowCovid").child("yes").set(firebase.database.ServerValue.increment(1));
+    } else {
+      getState(req).child("events").child("riskQuiz").child("knowCovid").child("no").set(firebase.database.ServerValue.increment(1));
+    }
+
+    //Mask
+    if (compStrings(req.body.quizResults.mask,'yes')) {
+      getState(req).child("events").child("riskQuiz").child("mask").child("yes").set(firebase.database.ServerValue.increment(1));
+    } else {
+      getState(req).child("events").child("riskQuiz").child("mask").child("no").set(firebase.database.ServerValue.increment(1));
+    }
+
+    //Home
+    if (compStrings(req.body.quizResults.home,'yes')) {
+      getState(req).child("events").child("riskQuiz").child("home").child("yes").set(firebase.database.ServerValue.increment(1));
+    } else {
+      getState(req).child("events").child("riskQuiz").child("home").child("no").set(firebase.database.ServerValue.increment(1));
+    }
+
+    //Sex
+    if (compStrings(req.body.quizResults.sex,'male')) {
+      getState(req).child("events").child("riskQuiz").child("sex").child("male").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.sex,'female')) {
+      getState(req).child("events").child("riskQuiz").child("sex").child("female").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.sex,'nonbinary')) {
+      getState(req).child("events").child("riskQuiz").child("sex").child("nonbinary").set(firebase.database.ServerValue.increment(1));
+    } else {
+      getState(req).child("events").child("riskQuiz").child("sex").child("other").set(firebase.database.ServerValue.increment(1));
+    }
+
+    //Protection
+    if (req.body.quizResults.protection != '') {
+      for (let i = req.body.quizResults.protection.split(',').length; i > 0; i--) {
+        getState(req).child("events").child("riskQuiz").child('protectiveMeasures').set(firebase.database.ServerValue.increment(1));
+      }
+    }
+
+    //Age
+    if (compStrings(req.body.quizResults.age,'1-5')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("1-5").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.age,'6-10')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("6-10").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.age,'11-15')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("11-15").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.age,'16-20')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("16-20").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.age,'21-25')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("21-25").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.age,'26-30')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("26-30").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.age,'31-35')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("31-35").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.age,'36-40')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("36-40").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.age,'41-45')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("41-45").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.age,'46-50')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("46-50").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.age,'51-55')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("51-55").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.age,'56-60')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("56-60").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.age,'61-65')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("61-65").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.age,'66-70')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("66-70").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.age,'71-75')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("71-75").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.age,'76-80')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("76-80").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.age,'81-85')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("81-85").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.age,'86-90')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("86-90").set(firebase.database.ServerValue.increment(1));
+    } else if (compStrings(req.body.quizResults.age,'91-95')) {
+      getState(req).child("events").child("riskQuiz").child("age").child("91-95").set(firebase.database.ServerValue.increment(1));
+    } else {
+      getState(req).child("events").child("riskQuiz").child("age").child("96-100").set(firebase.database.ServerValue.increment(1));
+    }
+    //NumTaken
+    getState(req).child("events").child("riskQuiz").child("numTaken").set(firebase.database.ServerValue.increment(1));
   }
 
+   //Infected
+   if (compStrings(req.body.quizResults.infected,'yes')) {
+    getCountry(req).child("events").child("riskQuiz").child("knowCovid").child("yes").set(firebase.database.ServerValue.increment(1));
+  } else {
+    getCountry(req).child("events").child("riskQuiz").child("knowCovid").child("no").set(firebase.database.ServerValue.increment(1));
+  }
+
+  //Mask
+  if (compStrings(req.body.quizResults.mask,'yes')) {
+    getCountry(req).child("events").child("riskQuiz").child("mask").child("yes").set(firebase.database.ServerValue.increment(1));
+  } else {
+    getCountry(req).child("events").child("riskQuiz").child("mask").child("no").set(firebase.database.ServerValue.increment(1));
+  }
+
+  //Home
+  if (compStrings(req.body.quizResults.home,'yes')) {
+    getCountry(req).child("events").child("riskQuiz").child("home").child("yes").set(firebase.database.ServerValue.increment(1));
+  } else {
+    getCountry(req).child("events").child("riskQuiz").child("home").child("no").set(firebase.database.ServerValue.increment(1));
+  }
+
+  //Sex
+  if (compStrings(req.body.quizResults.sex,'male')) {
+    getCountry(req).child("events").child("riskQuiz").child("sex").child("yes").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.sex,'female')) {
+    getCountry(req).child("events").child("riskQuiz").child("sex").child("female").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.sex,'nonbinary')) {
+    getCountry(req).child("events").child("riskQuiz").child("sex").child("nonbinary").set(firebase.database.ServerValue.increment(1));
+  } else {
+    getCountry(req).child("events").child("riskQuiz").child("sex").child("other").set(firebase.database.ServerValue.increment(1));
+  }
+
+  //Protection
+  if (req.body.quizResults.protection != '') {
+    for (let i = req.body.quizResults.protection.split(',').length; i > 0; i--) {
+      getCountry(req).child("events").child("riskQuiz").child('protectiveMeasures').set(firebase.database.ServerValue.increment(1));
+    }
+  }
+
+  //Age
+  if (compStrings(req.body.quizResults.age,'1-5')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("1-5").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.age,'6-10')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("6-10").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.age,'11-15')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("11-15").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.age,'16-20')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("16-20").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.age,'21-25')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("21-25").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.age,'26-30')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("26-30").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.age,'31-35')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("31-35").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.age,'36-40')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("36-40").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.age,'41-45')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("41-45").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.age,'46-50')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("46-50").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.age,'51-55')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("51-55").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.age,'56-60')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("56-60").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.age,'61-65')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("61-65").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.age,'66-70')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("66-70").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.age,'71-75')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("71-75").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.age,'76-80')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("76-80").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.age,'81-85')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("81-85").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.age,'86-90')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("86-90").set(firebase.database.ServerValue.increment(1));
+  } else if (compStrings(req.body.quizResults.age,'91-95')) {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("91-95").set(firebase.database.ServerValue.increment(1));
+  } else {
+    getCountry(req).child("events").child("riskQuiz").child("age").child("96-100").set(firebase.database.ServerValue.increment(1));
+  }
+  //NumTaken
+  getCountry(req).child("events").child("riskQuiz").child("numTaken").set(firebase.database.ServerValue.increment(1));
   //Increment the value by 1 for the country
   const countryQuiz = getCountry(req).child("events").child("riskQuiz");
 }
@@ -163,7 +336,7 @@ app.post("/updateMapLoads", (request, result) => {
 
 app.post("/updateRiskQuiz", (request, result) => {
   if (verifyCountry(request)) {
-
+    saveRiskQuizResult(request);
   }
 });
 
